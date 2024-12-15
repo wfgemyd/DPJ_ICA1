d29f785103dacf32ce2fc36a9e42a3726bbd3690
anyaachan <anna.danchenko@icloud.com>
2024-12-15 09:10:40 +0100

    add INetworkLayerProtocol Docstrings

--- src/com/myproject/daniel/INetworkInterface.java
+++ src/com/myproject/daniel/INetworkInterface.java
@@ -1,6 +1,11 @@
 package com.myproject.daniel;
 
+/**
+ * Represents a network interface of a network device.
+ */
+
 public interface INetworkInterface {
+
     void connect(IPhysicalMedium medium);
     void sendPacket(Packet packet);
     void receivePacket(Packet packet);
--- src/com/myproject/daniel/INetworkLayerProtocol.java
+++ src/com/myproject/daniel/INetworkLayerProtocol.java
@@ -1,6 +1,23 @@
 package com.myproject.daniel;
 
+/**
+ * The INetworkLayerProtocol defines the behavior of a network layer protocol (e.g. IPv4, IPv6).
+ * It is responsible for handling packets,
+ * including determining the next appropriate interface or device through
+ * which the packet should be sent based on the destination address.
+ */
 public interface INetworkLayerProtocol {
+    /**
+     * Routes a packet to the appropriate interface/device based on the destination address.
+     *
+     * @param packet Packet instance containing data, destination and headers necessary to conduct network transmission.
+     * @param sourceInterface the INetworkInterface from which the packet came.
+     *
+     */
     void routePacket(Packet packet, INetworkInterface sourceInterface);
+
+    /**
+     * Returns the address associated with specific network layer protocol.
+     */
     String getAddress();
 }
